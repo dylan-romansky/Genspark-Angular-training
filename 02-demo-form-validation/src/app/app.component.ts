65b0444cab338bc2718e1ea2baf6a6b690a70641
bigdiggysquiggle <dylanromansky@yahoo.com>
2022-11-17 13:47:08 -0800

    still working on nailing down the concepts

--- 02-demo-form-validation/src/app/app.component.ts
+++ 02-demo-form-validation/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'form-validation';
+  title = 'Reactive Form';
+  loginform = new FormGroup({
+  user: new FormControl('', Validators.required),
+  password: new FormControl <number | string>('', {nonNullable:true}),
+  phone: new FormControl('', Validators.required),
+  favouriteWord: new FormControl('', Validators.required)
+  });
+
+  get email() {
+  	return this.loginform.get('email');
+  }
+  loginconfirm() {
+  	console.log(this.loginform.value);
+  }
 }
